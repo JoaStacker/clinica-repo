005c2b75bdd1478f402334a960a077f9973973c3
Joaquin E. Sarmiento <joaquin.sarmiento@ieee.org>
2024-08-29 15:04:35 -0300

    docker compose structure.

--- Clinica.Api/Program.cs
+++ Clinica.Api/Program.cs
@@ -1,10 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://*:80"); // Bind to port 8080
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+/* Database Context Dependency Injection */
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
+// builder.Services.AddDbContext<DatabaseContext>(opt => opt.UseSqlServer(connectionString));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
