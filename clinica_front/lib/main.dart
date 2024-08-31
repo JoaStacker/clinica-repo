d6e62f85c3409fceb9a8b3e9ec52f8ae3e13efb9
Joaquin E. Sarmiento <joaquin.sarmiento@ieee.org>
2024-08-31 02:05:11 -0300

    quick fix frontend

--- clinica_front/lib/main.dart
+++ clinica_front/lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-const BASE_URL = 'http://localhost:8001'
+const BASE_URL = 'http://localhost:8001';
 
 void main() => runApp(MyApp());
 
