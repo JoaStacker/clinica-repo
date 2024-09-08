58cedc01b84747d8979326aee1db22a871a59868
Matias Alicata <matiasalicata@gmail.com>
2024-09-08 00:42:46 -0300

    Add inheritance

---
+++ Clinica.Api/Entities/Medico.cs
@@ -0,0 +1,7 @@
+﻿namespace Clinica.Api.Entities
+{
+    public class Medico : Persona
+    {
+        public int Matricula { get; set; }
+    }
+}
--- Clinica.Api/Entities/Paciente.cs
+++ Clinica.Api/Entities/Paciente.cs
@@ -2,5 +2,9 @@
 {
     public class Paciente : Persona
     {
+        public int NroAfiliado { get; set; }
+        public int NroPasaporte { get; set; }
+        public DateTime FechaDefunsion { get; set; }
+        public bool Estado { get; set; }
     }
 }
