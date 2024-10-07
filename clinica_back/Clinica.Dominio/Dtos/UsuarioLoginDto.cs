4d00a3692e21ba12a51edb179fa400ed583c7975
Matias Alicata <matiasalicata@gmail.com>
2024-10-07 11:14:05 -0300

    Se elimina propiedad name

--- clinica_back/Clinica.Dominio/Dtos/UsuarioLoginDto.cs
+++ clinica_back/Clinica.Dominio/Dtos/UsuarioLoginDto.cs
@@ -8,7 +8,6 @@ namespace Clinica.Dominio.Dtos
 {
     public class UsuarioLoginDto
     {
-        public string Name { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
     }
