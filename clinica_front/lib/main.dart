00b3b00d17902394bb9cf1b11fa8f71028a18a0e
Ignacio Esteban Fonts <ignacio.fonts@banana-software.com>
2024-10-11 15:54:35 -0300

    Intentando reparar Ruteado

--- clinica_front/lib/main.dart
+++ clinica_front/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:clinica_front/router/router_view_model.dart';
 import 'package:clinica_front/services/autentication.dart';
 import 'package:clinica_front/services/navegation_router.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -21,17 +22,36 @@ class CentroClinico extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
+        } else if (snapshot.hasError || !snapshot.hasData) {
           return const Center(child: Text('Error loading authentication status'));
         }
+
+        final isAuthenticated = snapshot.data!;
+        final localRouter = GoRouter(
+          navigatorKey: NavigatorKey().key, // Use global navigator key
+          initialLocation: isAuthenticated.isEmpty ? '/home' : '/login', // Set initial route based on auth status
+          redirect: (context, state) {
+            // If the user is not authenticated, always redirect to login
+            final bool loggingIn = state.fullPath!.contains('login');
+            if (isAuthenticated.isEmpty || !loggingIn) {
+              return '/login';
+            }
+            // If the user is authenticated and trying to go to login, redirect to home
+            if (isAuthenticated.isNotEmpty || loggingIn) {
+              return '/home';
+            }
+            return null; // No redirection needed
+          },
+          routes: router, // Your list of routes
+        );
+
         return MultiProvider(
           providers: routerViewModel,
-          child: MaterialApp(
-            navigatorKey: NavigatorKey().key,
+          child: MaterialApp.router(
+            key: NavigatorKey().key,
+            routerConfig: localRouter, // Pass the routerConfig instead of individual delegates
             debugShowCheckedModeBanner: false,
             title: 'Centro Clínico',
-            initialRoute: snapshot.data!.isEmpty ? '/login' : '/home',
-            routes: routes,
           ),
         );
       },
--- clinica_front/lib/router/router.dart
+++ clinica_front/lib/router/router.dart
@@ -2,10 +2,23 @@ import 'package:clinica_front/ui/pages/detail_patient/detail_patient_page.dart';
 import 'package:clinica_front/ui/pages/edit/edit_patient_page.dart';
 import 'package:clinica_front/ui/pages/home/home_page.dart';
 import 'package:clinica_front/ui/pages/login/login_page.dart';
+import 'package:go_router/go_router.dart';
 
-final routes = {
-  '/login': (context) => const LoginPage(),
-  '/home': (context) => HomePage(),
-  '/edit': (context) => EditPatientPage(),
-  '/patient': (context) => DetailPatientPage()
-};
+final List<RouteBase> router = [
+  GoRoute(
+    path: '/login',
+    builder: (context, state) => const LoginPage(),
+  ),
+  GoRoute(
+    path: '/home',
+    builder: (context, state) => HomePage(),
+  ),
+  GoRoute(
+    path: '/edit',
+    builder: (context, state) => EditPatientPage(),
+  ),
+  GoRoute(
+    path: '/patient',
+    builder: (context, state) => DetailPatientPage(),
+  ),
+];
