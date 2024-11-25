696fb7a797c3c4f6c02fd52430fcfd9205284da5
Ignacio Esteban Fonts <ignacio.fonts@banana-software.com>
2024-11-25 11:07:56 -0300

    Se agrega alerta endpoint nuevo diagnostico y pedidos de laboratorio

--- clinica_front/lib/domain/repository/diagnosticos_repository.dart
+++ clinica_front/lib/domain/repository/diagnosticos_repository.dart
@@ -1,6 +1,7 @@
 import 'package:clinica_front/data/model/diagnostico.dart';
+import 'package:http/http.dart' as http;
 
 abstract class DiagnosticosRepository {
   Future<List<Diagnostico>> getDiagnosticoPrevio(int idPaciente);
-
+  Future<http.Response> saveNuevoDiagnostico(int idPaciente, String enfermedad);
 }
